c90181edd37fa6fa6614c82ecc67dd4369914262
adriennemadarang <adriennemadarang@icloud.com>
2023-08-14 17:49:06 -0700

    updated server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./controller');
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listenerCount(PORT, () => console.log('Now listening'));
+});
