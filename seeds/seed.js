4164f226d6c4a930a640dd905e4b9d1c9d7dfa63
Aidancm1 <aidancmorrow1@gmail.com>
2023-08-14 20:34:50 -0400

    seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,17 @@
-const sequelize = require('../config/connection');
-const { Budget, User } = require ('../models');
+const sequelize = require("../config/connection");
+const { Budget, User } = require("../models");
 
-const budgetSeedData = require('./budgetSeedData.json');
-const userSeedData = require('./userSeedData.json');
+const budgetSeedData = require("./budgetSeedData.json");
+const userSeedData = require("./userSeedData.json");
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-    const budget = await Budget.bulkCreate
-    (budgetSeedData, {
-        individualHooks: true,
-        returning: true,
-    });
-    
-}
+  const budget = await Budget.bulkCreate(budgetSeedData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
+};
 
+seedDatabase();
